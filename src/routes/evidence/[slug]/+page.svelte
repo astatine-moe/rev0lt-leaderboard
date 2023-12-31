63698bb8196aea769b18c31c9b82d3ff59610867
Morgan Evans <morgan@morganevans.dev>
2023-12-31 19:48:49 +0000

    Added evidence tracking

---
+++ src/routes/evidence/[slug]/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+    export let data;
+    import { PUBLIC_API_URL } from "$env/static/public";
+    import { copy } from "svelte-copy";
+
+    let images = [];
+    let user = {};
+    let err = null;
+
+    let loading = true;
+
+    import { Spinner } from "flowbite-svelte";
+    import { onMount } from "svelte";
+
+    const get = async () => {
+        try {
+            const res = await fetch(`${PUBLIC_API_URL}/evidence/${data.slug}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (res.ok) {
+                let responseData = await res.json();
+                images = responseData.images;
+                user = responseData.user;
+            } else {
+                err = "failed to load";
+            }
+        } catch (e) {
+            console.error(e);
+            err = "failed to load";
+        }
+    };
+
+    onMount(async () => {
+        loading = true;
+        await get();
+        loading = false;
+    });
+</script>
+
+{#if loading}
+    <div class="text-center">
+        <Spinner size={16} class="mt-5" />
+    </div>
+{:else if err}
+    <h1>Error fetching evidence</h1>
+{:else}
+    <div class="container mt-5">
+        <h1 class="text-xl mb-3">
+            Evidence for <span
+                class="mention"
+                use:copy={`<@${user.identifier}>`}>@{user.username}</span
+            >
+        </h1>
+        {#each images as image, i}
+            <img src={`${PUBLIC_API_URL}${image}`} alt="evidence {i + 1}" />
+        {/each}
+    </div>
+{/if}
+
+<style lang="scss">
+    .mention {
+        border-radius: 3px;
+        padding: 0 2px;
+
+        font-weight: 500;
+        unicode-bidi: plaintext;
+        color: rgb(201, 205, 251);
+        outline: rgb(201, 205, 251);
+        background: rgba(88, 101, 242, 0.3);
+
+        cursor: pointer;
+        &:hover {
+            background: rgb(88, 101, 242);
+            text-decoration: underline;
+        }
+    }
+</style>
