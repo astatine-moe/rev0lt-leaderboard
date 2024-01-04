bd09f38bcd3be3c24f87c4fd30276e39bef29381
Morgan Evans <morgan@morganevans.dev>
2024-01-04 16:14:43 +0000

    Output to fit flowbite

--- src/lib/client/paginate.js
+++ src/lib/client/paginate.js
@@ -1,4 +1,6 @@
 export default (arr, perPage = 5) =>
-    Array.from({ length: Math.ceil(arr.length / perPage) }, (_, i) =>
-        arr.slice(i * perPage, (i + 1) * perPage)
-    );
+    Array.from({ length: Math.ceil(arr.length / perPage) }, (_, i) => ({
+        name: i + 1,
+        href: `/?page=${i + 1}`,
+        items: arr.slice(i * perPage, (i + 1) * perPage),
+    }));
